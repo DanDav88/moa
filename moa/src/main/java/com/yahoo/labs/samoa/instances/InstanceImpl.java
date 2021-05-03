922f69202e7314ae28f0b1ccd6f0d4e71ec12892
DanDav88 <26633017+DanDav88@users.noreply.github.com>
2021-05-03 22:50:01 +0200

    fix InstanceImpl when deleteAttributeAt method is called

--- moa/src/main/java/com/yahoo/labs/samoa/instances/InstanceImpl.java
+++ moa/src/main/java/com/yahoo/labs/samoa/instances/InstanceImpl.java
@@ -1,17 +1,17 @@
 /*
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * 	        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific
  * language governing permissions and limitations under the
- * License.  
+ * License.
  */
 package com.yahoo.labs.samoa.instances;
 
@@ -131,7 +131,7 @@ public class InstanceImpl implements MultiLabelInstance {
     public int indexOfAttribute(Attribute attribute){
         return this.instanceHeader.indexOf(attribute);
     }
-    
+
     /**
      * Delete attribute at.
      *
@@ -366,9 +366,11 @@ public class InstanceImpl implements MultiLabelInstance {
     public void setDataset(Instances dataset) {
         if(dataset instanceof InstancesHeader) {
             this.instanceHeader = (InstancesHeader) dataset;
-        }else {
-            this.instanceHeader = new InstancesHeader(dataset);
+        }else if(dataset == null) {
+            this.instanceHeader = null;
         }
+        else
+            this.instanceHeader = new InstancesHeader(dataset);
     }
 
     /**
