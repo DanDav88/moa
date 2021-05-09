cfa0e3412d239d4390749674c08173df0341d306
DanDav88 <26633017+DanDav88@users.noreply.github.com>
2021-05-09 14:57:23 +0200

    update AdaptiveQuickReductTest

--- moa/src/test/java/moa/tasks/adaptive_quick_reduct/service/AdaptiveQuickReductTest.java
+++ moa/src/test/java/moa/tasks/adaptive_quick_reduct/service/AdaptiveQuickReductTest.java
@@ -3,20 +3,22 @@ package moa.tasks.adaptive_quick_reduct.service;
 import com.yahoo.labs.samoa.instances.Instance;
 import com.yahoo.labs.samoa.instances.Instances;
 import moa.classifiers.AbstractClassifier;
+import moa.classifiers.bayes.NaiveBayes;
 import moa.classifiers.lazy.kNN;
 import moa.tasks.AdaptiveQuickReductConfig;
 import moa.tasks.adaptive_quick_reduct.model.Reduct;
 import moa.tasks.adaptive_quick_reduct.model.SlidingWindow;
 import moa.tasks.adaptive_quick_reduct.model.Window;
 import moa.tasks.adaptive_quick_reduct.model.distance_utils.DistanceCalculatorAbstract;
+import moa.tasks.adaptive_quick_reduct.model.distance_utils.EuclideanDistanceCalculator;
 import moa.tasks.adaptive_quick_reduct.model.distance_utils.ManhattanDistanceCalculator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.time.ZonedDateTime;
+import java.util.Optional;
 import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -41,48 +43,120 @@ public class AdaptiveQuickReductTest {
 
     Instances instances = readInstances(datasetFileName);
 
-    logger.info("Adaptive Quick Reduct Config");
+    logger.info("Adaptive Quick Reduct TEST");
     logger.info(String.format("Dataset %s, num instances %d, num classes %d",
             instances.getRelationName(), instances.numInstances(), instances.numClasses()));
 
-    Window<Instance> windowsAqr = new SlidingWindow<>(instances.numInstances(), DEFAULT_WINDOWS_SIZE, DEFAULT_WINDOWS_OVERLAP);
-    Window<Instance> windowsStd = new SlidingWindow<>(instances.numInstances(), DEFAULT_WINDOWS_SIZE, DEFAULT_WINDOWS_OVERLAP);
-//    DistanceCalculatorAbstract distanceCalculator = new EuclideanDistanceCalculator();
-    DistanceCalculatorAbstract distanceCalculator = new ManhattanDistanceCalculator();
-    AdaptiveQuickReduct aqr = new AdaptiveQuickReduct(instances, distanceCalculator, DEFAULT_SIMILARITY_THRESHOLD);
 
-    logger.info(String.format("Window Size %d, windows overlap %d, num iteration %d, distance type %s, similarity threshold %f",
-            DEFAULT_WINDOWS_SIZE, DEFAULT_WINDOWS_OVERLAP, windowsAqr.getTotalIterationNumber(),
-            distanceCalculator.getClass().getSimpleName(), DEFAULT_SIMILARITY_THRESHOLD));
-
-//    System.out.println(copyInst);
-
-//    AbstractClassifier classifier = new kNN();
-//    runClassificationStandard(instances, windowsStd, classifier);
-//    AbstractClassifier classifierAQR = new kNN();
-//    runClassificationAQR(instances, aqr, windowsAqr, classifierAQR);
-
-    ExecutorService executor1 = Executors.newSingleThreadExecutor();
-    executor1.submit(() -> {
-      AbstractClassifier classifier = new kNN();
-      runClassificationStandard(instances, windowsStd, classifier);
+    ExecutorService e1 = Executors.newFixedThreadPool(6);
+    e1.submit(() -> {
+      runBayesClassifications(instances, 100, 90, DEFAULT_SIMILARITY_THRESHOLD);
+      runBayesClassifications(instances, 500, 250, DEFAULT_SIMILARITY_THRESHOLD);
     });
 
-    ExecutorService executor2 = Executors.newSingleThreadExecutor();
-    executor2.submit(() -> {
-      AbstractClassifier classifierAQR = new kNN();
-      runClassificationAQR(instances, aqr, windowsAqr, classifierAQR);
+    ExecutorService e2 = Executors.newFixedThreadPool(6);
+    e2.submit(() -> {
+      runBayesClassifications(instances, 200, 100, DEFAULT_SIMILARITY_THRESHOLD);
+      runBayesClassifications(instances, 500, 100, DEFAULT_SIMILARITY_THRESHOLD);
+      runBayesClassifications(instances, 500, 0, DEFAULT_SIMILARITY_THRESHOLD);
+      runBayesClassifications(instances, 1000, 0, DEFAULT_SIMILARITY_THRESHOLD);
     });
-    try {
-      executor1.wait();
-      executor2.wait();
-    } catch(InterruptedException e) {
-      e.printStackTrace();
-    }
+
+    ExecutorService e3 = Executors.newFixedThreadPool(6);
+    e3.submit(() -> {
+      runKNNClassifications(instances, 100, 90, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+      runKNNClassifications(instances, 500, 250, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+    });
+
+    ExecutorService e4 = Executors.newFixedThreadPool(6);
+    e4.submit(() -> {
+      runKNNClassifications(instances, 200, 100, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+      runKNNClassifications(instances, 500, 100, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+      runKNNClassifications(instances, 500, 0, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+      runKNNClassifications(instances, 1000, 0, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+      ;
+    });
+
+//    runBayesClassifications(instances, 100, 90, DEFAULT_SIMILARITY_THRESHOLD);
+//    runBayesClassifications(instances, 200, 100, DEFAULT_SIMILARITY_THRESHOLD);
+//    runBayesClassifications(instances, 500, 250, DEFAULT_SIMILARITY_THRESHOLD);
+//    runBayesClassifications(instances, 500, 100, DEFAULT_SIMILARITY_THRESHOLD);
+//    runBayesClassifications(instances, 500, 0, DEFAULT_SIMILARITY_THRESHOLD);
+//    runBayesClassifications(instances, 1000, 0, DEFAULT_SIMILARITY_THRESHOLD);
+//
+//    runKNNClassifications(instances, 100, 90, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+//    runKNNClassifications(instances, 200, 100, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+//    runKNNClassifications(instances, 500, 250, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+//    runKNNClassifications(instances, 500, 100, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+//    runKNNClassifications(instances, 500, 0, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+//    runKNNClassifications(instances, 1000, 0, DEFAULT_SIMILARITY_THRESHOLD, kNN.K_VALUE_DEFAULT, kNN.STORED_INSTANCES_DEFAULT);
+
 
   }
 
-  private static void runClassificationAQR(Instances instances, AdaptiveQuickReduct aqr, Window<Instance> windows, AbstractClassifier classifier) {
+  private static void runKNNClassifications(Instances instances, int windowSize, int windowOverlap, double similarityThreshold, int kN, int storedInstances) {
+//    DistanceCalculatorAbstract distanceCalculator = new ManhattanDistanceCalculator();
+    DistanceCalculatorAbstract distanceCalculator = new EuclideanDistanceCalculator();
+    AdaptiveQuickReduct aqr = new AdaptiveQuickReduct(instances, distanceCalculator, similarityThreshold);
+    Window<Instance> windowsAqr = new SlidingWindow<>(instances.numInstances(), windowSize, windowOverlap);
+    AbstractClassifier classifierAQR = new kNN(Optional.of(kN), Optional.of(storedInstances));
+
+    logger.info(String.format("Window Size %d, windows overlap %d, num iteration %d, K Neighbor %d, KNN Stored Instances %d, AQR distance type %s, AQR similarity threshold %f",
+            windowSize, windowOverlap, windowsAqr.getTotalIterationNumber(), kN, storedInstances,
+            distanceCalculator.getClass().getSimpleName(), similarityThreshold));
+
+    Window<Instance> windowsStd = new SlidingWindow<>(instances.numInstances(), windowSize, windowOverlap);
+    AbstractClassifier classifier = new kNN(Optional.of(kN), Optional.of(storedInstances));
+
+    ArrayList<Double> accuracies = runClassificationStandard(instances, windowsStd, classifier);
+    ArrayList<Double> accuraciesAQR = runClassificationAQR(instances, aqr, windowsAqr, classifierAQR);
+
+    String relationName = String.join("_", instances.getRelationName().split(" "));
+    String timeStamp = ZonedDateTime.now().format(formatter_yyyyMMdd_HH_mm_ss);
+    String configInfo = String.format("ws_%d_wo_%d_k_%d_si_%d", windowSize, windowOverlap, kN, storedInstances);
+
+    String filenameAccuraciesAQR = String.format("moa/CSV/AQR_KNN_ACC_%s_%s_%s.csv", relationName, configInfo, timeStamp);
+    exportAccuraciesCSV(accuraciesAQR, filenameAccuraciesAQR);
+    logger.info(String.format("Exported %s ", filenameAccuraciesAQR));
+
+    String filenameAccuracies = String.format("moa/CSV/STD_KNN_ACC_%s_%s_%s.csv", relationName, configInfo, timeStamp);
+    exportAccuraciesCSV(accuracies, filenameAccuracies);
+    logger.info(String.format("Exported %s ", filenameAccuracies));
+  }
+
+  private static void runBayesClassifications(Instances instances, int windowSize, int windowOverlap, double similarityThreshold) {
+//    DistanceCalculatorAbstract distanceCalculator = new ManhattanDistanceCalculator();
+    DistanceCalculatorAbstract distanceCalculator = new EuclideanDistanceCalculator();
+    AdaptiveQuickReduct aqr = new AdaptiveQuickReduct(instances, distanceCalculator, similarityThreshold);
+    Window<Instance> windowsAqr = new SlidingWindow<>(instances.numInstances(), windowSize, windowOverlap);
+    AbstractClassifier classifierAQR = new NaiveBayes();
+    classifierAQR.resetLearningImpl();
+
+    logger.info(String.format("Window Size %d, windows overlap %d, num iteration %d, distance type %s, similarity threshold %f",
+            windowSize, windowOverlap, windowsAqr.getTotalIterationNumber(),
+            distanceCalculator.getClass().getSimpleName(), similarityThreshold));
+
+    Window<Instance> windowsStd = new SlidingWindow<>(instances.numInstances(), windowSize, windowOverlap);
+    AbstractClassifier classifier = new NaiveBayes();
+    classifier.resetLearningImpl();
+
+    ArrayList<Double> accuracies = runClassificationStandard(instances, windowsStd, classifier);
+    ArrayList<Double> accuraciesAQR = runClassificationAQR(instances, aqr, windowsAqr, classifierAQR);
+
+    String relationName = String.join("_", instances.getRelationName().split(" "));
+    String timeStamp = ZonedDateTime.now().format(formatter_yyyyMMdd_HH_mm_ss);
+    String configInfo = String.format("ws_%d_wo_%d", windowSize, windowOverlap);
+
+    String filenameAccuraciesAQR = String.format("moa/CSV/AQR_BAYES_ACC_%s_%s_%s.csv", relationName, configInfo, timeStamp);
+    exportAccuraciesCSV(accuraciesAQR, filenameAccuraciesAQR);
+    logger.info(String.format("Exported %s ", filenameAccuraciesAQR));
+
+    String filenameAccuracies = String.format("moa/CSV/STD_BAYES_ACC_%s_%s_%s.csv", relationName, configInfo, timeStamp);
+    exportAccuraciesCSV(accuracies, filenameAccuracies);
+    logger.info(String.format("Exported %s ", filenameAccuracies));
+  }
+
+  private static ArrayList<Double> runClassificationAQR(Instances instances, AdaptiveQuickReduct aqr, Window<Instance> windows, AbstractClassifier classifier) {
 
     int iterationNumber = 1;
 
@@ -99,15 +173,25 @@ public class AdaptiveQuickReductTest {
       //--------------------------------------TEST----------------------------------------------------------------------
       if(iterationNumber == 1) {
         //test on full window
-        currentOverallAccuracy = getAccuracyValue(classifier, iWindow, instances.numClasses(), AQR_CLASSIFICATION);
+        try {
+          currentOverallAccuracy = getAccuracyValue(classifier, iWindow, instances.numClasses(), AQR_CLASSIFICATION);
+        } catch(NullPointerException npe) {
+          logger.warn("getAccuracyValue: throws NULL pointer exception");
+          currentOverallAccuracy = 0.0;
+        }
       }
       else {
         //test on window with previous reduct
         Instances copyInstances = new Instances(instances);
         removeAttributesFromInstances(copyInstances, previousReduct.getReductSet());
-
         ArrayList<AbstractMap.SimpleEntry<Integer, Instance>> reductWindow = windows.getCurrentWindow(copyInstances.getInstances());
-        currentOverallAccuracy = getAccuracyValue(classifier, reductWindow, instances.numClasses(), AQR_CLASSIFICATION);
+
+        try {
+          currentOverallAccuracy = getAccuracyValue(classifier, reductWindow, instances.numClasses(), AQR_CLASSIFICATION);
+        } catch(NullPointerException npe) {
+          logger.warn("getAccuracyValue: throws NULL pointer exception");
+          currentOverallAccuracy = 0.0;
+        }
       }
 
       accuracies.add(currentOverallAccuracy);
@@ -139,21 +223,16 @@ public class AdaptiveQuickReductTest {
 
     double[][] scores = getAttributeScoresFromReducts(reducts, instances);
 
-    String filenameScores = String.format("moa/CSV/Scores_%s_%s.csv",
-            String.join("_", instances.getRelationName().split(" ")),
-            ZonedDateTime.now().format(formatter_yyyyMMdd_HH_mm_ss));
+    String relationName = String.join("_", instances.getRelationName().split(" "));
+    String timestamp = ZonedDateTime.now().format(formatter_yyyyMMdd_HH_mm_ss);
+    String filenameScores = String.format("moa/CSV/Scores_%s_%s_%s.csv", relationName, windows, timestamp);
     exportAttributeScoresCSV(scores, instances, filenameScores);
     logger.info(String.format("Exported %s ", filenameScores));
 
-    String filenameAccuracies = String.format("moa/CSV/AQR_Accuracies_%s_%s.csv",
-            String.join("_", instances.getRelationName().split(" ")),
-            ZonedDateTime.now().format(formatter_yyyyMMdd_HH_mm_ss));
-
-    exportAccuraciesCSV(accuracies,filenameAccuracies);
-    logger.info(String.format("Exported %s ", filenameAccuracies));
+    return accuracies;
   }
 
-  private static void runClassificationStandard(Instances instances, Window<Instance> windows, AbstractClassifier classifier) {
+  private static ArrayList<Double> runClassificationStandard(Instances instances, Window<Instance> windows, AbstractClassifier classifier) {
 
     int iterationNumber = 1;
 
@@ -166,7 +245,13 @@ public class AdaptiveQuickReductTest {
     while(!iWindow.isEmpty()) {
 
       //--------------------------------------TEST----------------------------------------------------------------------
-      double currentOverallAccuracy = getAccuracyValue(classifier, iWindow, instances.numClasses(), NO_AQR_CLASSIFICATION);
+      double currentOverallAccuracy;
+      try {
+        currentOverallAccuracy = getAccuracyValue(classifier, iWindow, instances.numClasses(), NO_AQR_CLASSIFICATION);
+      } catch(NullPointerException npe) {
+        logger.warn("getAccuracyValue: throws NULL pointer exception");
+        currentOverallAccuracy = 0.0;
+      }
 
       accuracies.add(currentOverallAccuracy);
       logger.info(String.format("%s iteration n. %d, Current Accuracy = %f", NO_AQR_CLASSIFICATION, iterationNumber, currentOverallAccuracy));
@@ -184,69 +269,7 @@ public class AdaptiveQuickReductTest {
 
     logger.info("Classification - Ending Computation");
 
-    String filenameAccuracies = String.format("moa/CSV/Classifier_Accuracies_%s_%s.csv",
-            String.join("_", instances.getRelationName().split(" ")),
-            ZonedDateTime.now().format(formatter_yyyyMMdd_HH_mm_ss));
-
-    exportAccuraciesCSV(accuracies,filenameAccuracies);
-    logger.info(String.format("Exported %s ", filenameAccuracies));
-  }
-
-  public static double getAccuracyValue(AbstractClassifier classifier,
-                                        ArrayList<AbstractMap.SimpleEntry<Integer, Instance>> iWindow,
-                                        int numClasses,
-                                        String jobName) {
-    int[] truePositive = new int[numClasses];
-    int[] trueNegative = new int[numClasses];
-    int[] falsePositive = new int[numClasses];
-    int[] falseNegative = new int[numClasses];
-    for(int i = 0; i < numClasses; i++) {
-      truePositive[i] = 0;
-      trueNegative[i] = 0;
-      falsePositive[i] = 0;
-      falseNegative[i] = 0;
-    }
-
-    iWindow.forEach(keyValue -> {
-      int predictedIndex = moa.core.Utils.maxIndex(classifier.getVotesForInstance(keyValue.getValue()));
-      int correctIndex = (int) keyValue.getValue().classValue();
-      if(predictedIndex == correctIndex) {
-        truePositive[correctIndex]++;
-        for(int i = 0; i < numClasses; i++) {
-          if(i != correctIndex)
-            trueNegative[i]++;
-        }
-      }
-      else {
-        falsePositive[predictedIndex]++;
-        falseNegative[correctIndex]++;
-        for(int i = 0; i < numClasses; i++) {
-          if(i != correctIndex && i != predictedIndex)
-            trueNegative[i]++;
-        }
-      }
-    });
-
-    int truePositiveSum = 0;
-    int trueNegativeSum = 0;
-    int falsePositiveSum = 0;
-    int falseNegativeSum = 0;
-
-    for(int i = 0; i < numClasses; i++) {
-      truePositiveSum += truePositive[i];
-      trueNegativeSum += trueNegative[i];
-      falsePositiveSum += falsePositive[i];
-      falseNegativeSum += falseNegative[i];
-
-      logger.debug(String.format("%s Class %d, TP = %d, TN = %d, FP = %d, FN = %d, Accuracy %f",
-              jobName, i, truePositive[i], trueNegative[i], falsePositive[i], falseNegative[i],
-              computeAccuracy(truePositive[i], trueNegative[i], falsePositive[i], falseNegative[i])));
-    }
-    return computeAccuracy(truePositiveSum, trueNegativeSum, falsePositiveSum, falseNegativeSum);
-  }
-
-  public static double computeAccuracy(int TP, int TN, int FP, int FN) {
-    return (double) (TP + TN) / (TP + TN + FN + FP);
+    return accuracies;
   }
 
 }
